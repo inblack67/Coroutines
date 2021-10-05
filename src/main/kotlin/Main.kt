52d65904b28118882504848d4b9ffa2415a579a3
aman <aman>
2021-10-05 16:40:23 +0530

    coroutineScope

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,20 @@
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-fun main () = runBlocking {
-    launch {
-        greetFromCoroutine()
-    }
-    println("main => hello")
+fun main() = runBlocking {
+    greetFromCoroutine()
 }
 
-suspend fun greetFromCoroutine() {
-    delay(1000L)
-    println("coroutine => hello")
+suspend fun greetFromCoroutine() = coroutineScope {
+    launch {
+        delay(1000L)
+        println("coroutine => hello")
+    }
+    println("I will be first")
 }
+
+//In addition to the coroutine scope provided by different builders, it is possible to declare your own scope using the coroutineScope builder. It creates a coroutine scope and does not complete until all launched children complete.
+//runBlocking and coroutineScope builders may look similar because they both wait for their body and all its children to complete. The main difference is that the runBlocking method blocks the current thread for waiting, while coroutineScope just suspends, releasing the underlying thread for other usages. Because of that difference, runBlocking is a regular function and coroutineScope is a suspending function.
+
